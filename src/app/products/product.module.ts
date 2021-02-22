f57cec62c08ae50b3fb34242ee52fb28fe085735
Olabanji Mayowa Emmanuel <mayowae@outlook.com>
2021-02-22 06:28:52 +0100

    First Commit - Includes Child Routes

---
+++ src/app/products/product.module.ts
@@ -0,0 +1,47 @@
+import { NgModule } from '@angular/core';
+
+import { ProductListComponent } from './product-list.component';
+import { ProductDetailComponent } from './product-detail.component';
+import { ProductEditComponent } from './product-edit/product-edit.component';
+import { ProductEditInfoComponent } from './product-edit/product-edit-info.component';
+import { ProductEditTagsComponent } from './product-edit/product-edit-tags.component';
+import { ProductResolver } from './product-resolver.service';
+
+import { SharedModule } from '../shared/shared.module';
+import { RouterModule } from '@angular/router';
+
+@NgModule({
+  imports: [
+    SharedModule,
+    RouterModule.forChild([
+      {
+        path:'products',
+        component: ProductListComponent,
+        data: {pageTitle: 'Products'}
+      },
+      {
+        path:'products/:id',
+        component: ProductDetailComponent,
+        resolve: {resolvedData: ProductResolver}
+      },
+      {
+        path:'products/:id/edit',
+        component: ProductEditComponent,
+        resolve: {resolvedData: ProductResolver},
+        children: [
+          {path: 'info', component: ProductEditInfoComponent},
+          {path: 'tags', component: ProductEditTagsComponent},
+          {path: '', redirectTo: 'info', pathMatch: 'full'}
+        ]
+      },
+    ])
+  ],
+  declarations: [
+    ProductListComponent,
+    ProductDetailComponent,
+    ProductEditComponent,
+    ProductEditInfoComponent,
+    ProductEditTagsComponent
+  ]
+})
+export class ProductModule { }
